e4e3e6a07cd498261027e4ea686d83da66d4f075
Lap ren <renataramirezeuzarraga@gmail.com>
2025-02-13 17:37:07 -0600

    act 10

---
+++ Actividades semana 4/act 10.py
@@ -0,0 +1,24 @@
+#10. Leer, escribir y modificar un archivo de texto.
+
+with open("archivo.txt", "w") as archivo:
+    archivo.write("linea 1: Hola, este es un archivo de texto.\n")
+    archivo.write("linea 2: quiero pasar programacion")
+
+# Leer el archivo (modo 'r' solo lectura)
+with open("archivo.txt", "r") as archivo:
+    contenido = archivo.read()
+    print("contenido del archivo:")
+    print(contenido)
+
+# Agregar más líneas al archivo (modo 'a' para añadir al final)
+with open("archivo.txt", "a") as archivo:
+    archivo.write("linea 3: podemos escribir aqui.\n")
+    archivo.write("linea 4: podemos seguir escribiendo aqui.\n")
+
+# Imprimir lo nuevo que se escribió
+print("lo nuevo que escribimos es:")
+
+# Leer el archivo nuevamente para mostrar lo agregado
+with open("archivo.txt", "r") as archivo:
+    contenido = archivo.read()
+    print(contenido)
